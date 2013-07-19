d0476af4aaa2d49c4a7d46f7c052f5ac63d84bbe
filipe <filipe@filipe-laptop>
2013-07-19 00:56:41 -0300

    Util

---
+++ dk-commons-service/ejbModule/com/dk/utils/constantes/ConstantesGenerica.java
@@ -0,0 +1,15 @@
+package com.dk.utils.constantes;
+
+public interface ConstantesGenerica {
+	
+	/**
+	 * Caminho da raiz para salvar pedidos.
+	 */
+	public String CAMINHO_SALVAR_ARQUIVOS_PEDIDOS = "/opt/checktag/pedidos/";
+	
+	/**
+	 * Caminho da raiz para salvar boletos.
+	 */
+	public String CAMINHO_SALVAR_ARQUIVOS_BOLETOS = "/opt/checktag/boletos/";
+
+}
